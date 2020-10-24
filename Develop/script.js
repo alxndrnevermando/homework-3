6f8db318e6c56f1d6a93d783a81d2ed29f0e5974
Armando Contreras <armandocontreras62596@gmail.com>
2020-10-23 20:58:50 -0700

    added generate password function which will generate a password, given the conditions presented to the user, in this case, checking off what criteria they want in their random password

--- Develop/script.js
+++ Develop/script.js
@@ -29,6 +29,28 @@ generateEl.addEventListener("click", function() {
     resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 });
 
+// Generate password function
+
+function generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length) {
+    //Init pw var
+    //Filter out unchecked types
+    //Loop over length call generator function for each type
+    //Add final pw to the pw var and return
+
+    let generatedPassword = "";
+
+    var typesCount = hasLower + hasUpper + hasNumber + hasSymbol;
+
+    var typesArr = [{hasLower}, {hasUpper}, {hasNumber}, {hasSymbol}].filter
+    (
+        item => Object.values(item)[0]
+    );
+
+    if(typesCount === 0) {
+        return '';
+    }
+}
+
 // Functions which generate string
 
 function getRandomLower() {
