a0e4056258eba30e1e3200fe7ebe8705e70b2669
Armando Contreras <armandocontreras62596@gmail.com>
2020-10-24 22:20:45 -0700

    added variables that take my confirmation prompts as a value

--- Develop/script.js
+++ Develop/script.js
@@ -4,3 +4,10 @@ var numbers = ("0123456789"). split("");
 var symbols = ("!@#$%^&*()_+"). split("");
 var lowers =  ("abcdefghijklmnopqrstuvwxyz"). split("");
 var uppers = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"). split("");
+
+// Tiffany Casey at https://github.com/TiffanyCasey/Password-Generator.git provided guidance on this assignment
+var confirmLength = "";
+var confirmSymbols;
+var confirmNumbers;
+var confirmUpper;
+var confirmLower;
